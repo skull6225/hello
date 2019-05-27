db9ab17dd3443996bc4df0fc2f524e8d3a288d6a
skull6225 <skull6225@gmail.com>
2019-05-27 12:13:34 +0300

    first commit

---
+++ src/com/test/Model.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Model {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public String setName(String name) {
+		return this.name = name;
+	}
+
+}
---
+++ src/com/test/hello.java
@@ -0,0 +1,49 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class hello extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+        
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    private Model model;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws IOException, ServletException {
+    	 String name = request.getParameter("name");
+    	 response.setContentType("text/html");
+         PrintWriter printWriter  = response.getWriter();
+         printWriter.println("<h1>"+"Hello word"+"</h1>");
+    }
+
+ 
+}
